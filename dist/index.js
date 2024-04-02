7d2a63d21b37a8834167c891efdcd26a165854af
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2024-04-02 12:51:31 -0400

    Update version to 1.2.0

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-fetch('https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@develop/dist/template.html').then((response) => {
+fetch('https://cdn.jsdelivr.net/gh/JamesRobertHugginsNgo/web-component-boilerplate@1.2.0/dist/template.html').then((response) => {
 	return response.text();
 }).then((templateString) => {
 	document.body.insertAdjacentHTML('beforeend', templateString);
