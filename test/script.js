1617b45606c4791a9965c327b509dd9ad63a7561
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2024-04-15 03:50:24 -0400

    Fix another bug with mutating component

--- test/script.js
+++ test/script.js
@@ -1,18 +1,22 @@
 import '../index.js';
 
-// const third = document.getElementById('third');
-// third.greeting = 'Holla';
+const third = document.getElementById('third');
+third.greeting = 'Holla';
 
-// const forth = document.getElementById('forth');
-// forth.items = [{ text: 'Home', link: '#' }];
+const forth = document.getElementById('forth');
+forth.items = [{ text: 'Home', link: '#' }];
 
 const sixth = document.getElementById('sixth');
+console.log('INNER HTML');
 sixth.innerHTML = /* html */ `
 	<a href="#">Home</a>
 	<a href="#">Directory</a>
 `;
-// sixth.remove();
+console.log('REMOVE');
+sixth.parentNode.removeChild(sixth);
+console.log('APPEND CHILD');
 const linkElement = sixth.appendChild(document.createElement('a'));
 linkElement.setAttribute('href', '#');
 linkElement.textContent = 'Page';
-// document.body.append(sixth);
+console.log('APPEND CHILD');
+document.body.appendChild(sixth);
